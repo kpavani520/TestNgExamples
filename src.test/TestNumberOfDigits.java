b790d0b6218c4cde889f0d15cdf65f382270bb7a
kpavani520 <kpavani520@gmail.com>
2022-04-04 12:14:19 -0700

    My changes

--- src.test/TestNumberOfDigits.java
+++ src.test/TestNumberOfDigits.java
@@ -5,11 +5,19 @@ import org.testng.annotations.Test;
 import com.examples.NumberOfDigits;
 
 public class TestNumberOfDigits {
+	NumberOfDigits nd = new NumberOfDigits();
 
 	@Test
 	public void test() {
-		NumberOfDigits nd = new NumberOfDigits();
+		
 		boolean actual = nd.isPalindrome(123);
 		Assert.assertTrue(!actual);
+	}
+	
+	@Test
+	public void reverseTest() {
+		int result=nd.reverseNum(123);
+		Assert.assertEquals(result, 321);
+		
 	}
 }
